69c950e2e6277b90fe6719fe929a0dd22de54a19
juntaod <juntao.d@outlook.com>
2013-10-16 14:18:29 +0800

    d

--- ejb/src/main/java/com/juniper/wtf/apps/service/NavService.java
+++ ejb/src/main/java/com/juniper/wtf/apps/service/NavService.java
@@ -2,10 +2,7 @@ package com.juniper.wtf.apps.service;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.management.Query;
 
 import com.juniper.wtf.apps.model.NavGroup;
 import com.juniper.wtf.apps.model.NavItem;
