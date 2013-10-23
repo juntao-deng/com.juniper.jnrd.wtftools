9c81a99cdc75fa6d652f7734b0e54518ebd36b3b
juntaod <juntao.d@outlook.com>
2013-10-23 13:03:52 +0800

    d

--- ejb/src/main/java/com/juniper/wtf/apps/model/NavType.java
+++
@@ -1,5 +0,0 @@
-package com.juniper.wtf.apps.model;
-
-public class NavType {
-
-}
--- web/src/main/java/com/juniper/wtf/tags/control/WtfControlConstants.java
+++
@@ -1,5 +0,0 @@
-package com.juniper.wtf.tags.control;
-
-public class WtfControlConstants {
-	public static final String CTRL_MENU = "menu";
-}
