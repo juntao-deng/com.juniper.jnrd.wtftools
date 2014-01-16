e0e56212898a5405a7128b1e4822c64721ff9b1d
juntao-deng <juntao.d@outlook.com>
2014-01-16 23:20:45 +0800

    d

--- src/net/juniper/jmp/persist/JmpPersistenceManager.java
+++ src/net/juniper/jmp/persist/JmpPersistenceManager.java
@@ -3,6 +3,8 @@ package net.juniper.jmp.persist;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.transaction.Synchronization;
+
 import net.juniper.jmp.persist.exp.JmpDbException;
 import net.juniper.jmp.persist.exp.JmpDbRuntimeException;
 import net.juniper.jmp.persist.impl.EntityPersistenceImpl;
@@ -10,13 +12,14 @@ import net.juniper.jmp.persist.jta.JtaSupportFactory;
 
 
 public abstract class JmpPersistenceManager {
-	private static ThreadLocal<Map<String, IJmpPersistence>> threadLocal = new ThreadLocal<Map<String, IJmpPersistence>>();
+	private static ThreadLocal<PersistenceWrapper> threadLocal = new ThreadLocal<PersistenceWrapper>();
 	private static Map<String, IJmpPersistence> persistenceMap = new HashMap<String, IJmpPersistence>();
     public static IJmpPersistence getInstance() {
     	IJmpPersistence instance = null;
     	if(JtaSupportFactory.getJtaSupport().hasTransaction()){
     		Object id = JtaSupportFactory.getJtaSupport().getTransactionId();
-    		instance = getNoTxInstance(id, null);
+    		instance = getTxInstance(id, null);
+    		JtaSupportFactory.getJtaSupport().registerSynchronization(new JmpTransactionSynchronization());
     	}
     	else{
     		instance = getNoTxInstance(null);
@@ -24,16 +27,6 @@ public abstract class JmpPersistenceManager {
     	return instance;
     }
     
-    
-    private static IJmpPersistence getNoTxInstance(String dsName) {
-		return null;
-	}
-
-
-	private static IJmpPersistence getNoTxInstance(Object id, String dsName) {
-		return null;
-	}
-
 	public static IJmpPersistence getInstance(String dsName){
         try {
 			return new EntityPersistenceImpl(dsName);
@@ -42,4 +35,49 @@ public abstract class JmpPersistenceManager {
         	throw new JmpDbRuntimeException(e);
         }
     }
+	
+	private static IJmpPersistence getNoTxInstance(String dsName) {
+		PersistenceWrapper wrapper = getPersistenceWrapper();
+		IJmpPersistence persistence = wrapper.getNoTxPersistence(dsName);
+		
+		return null;
+	}
+	
+	private static PersistenceWrapper getPersistenceWrapper() {
+		PersistenceWrapper wrapper = threadLocal.get();
+		if(wrapper == null){
+			wrapper = new PersistenceWrapper();
+			threadLocal.set(wrapper);
+		}
+		return wrapper;
+	}
+	
+	
+	private static IJmpPersistence getTxInstance(Object id, String dsName) {
+		PersistenceWrapper wrapper = getPersistenceWrapper();
+		if(wrapper.getSynchronization() == null){
+			Synchronization sync = new JmpTransactionSynchronization();
+			wrapper.setSynchronization(sync);
+		}
+		return null;
+	}
+
+	public static void releaseTxPersistence() {
+		
+	}
+	
+}
+class PersistenceWrapper{
+	private Map<String, IJmpPersistence> txPersistenceMap;
+	private IJmpPersistence noTxPersistence;
+	private Synchronization synchronization;
+	protected IJmpPersistence getNoTxPersistence() {
+		return noTxPersistence;
+	}
+	public void setSynchronization(Synchronization sync) {
+		this.synchronization = sync;
+	}
+	public Synchronization getSynchronization() {
+		return synchronization;
+	}
 }
