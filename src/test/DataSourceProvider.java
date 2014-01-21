4076114045f49d72d06b63bd3af9995bb0c333e6
juntao-deng <juntao.d@outlook.com>
2014-01-21 10:48:39 +0800

    d

--- src/test/DataSourceProvider.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import javax.sql.DataSource;
-import javax.sql.XADataSource;
-
-public interface DataSourceProvider {
-	public DataSource getDataSource(String name);
-	public XADataSource getXaDataSource(String name);
-}
